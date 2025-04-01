9a41fdcb3b61652392ab50b34bee5e185005f8a1
CptTemplar1 <michal2001k@op.pl>
2025-04-01 11:55:51 +0200

    Dodano wstępny schemat sieci (bez configu)

--- KonfiguracjaSieci_opisowo.sh
+++ KonfiguracjaSieci_opisowo.sh
@@ -1,3 +1,25 @@
+Wymagania projektowe
+    dostępy SSH
+    dynamiczny protokoły routingu (OSPF, EIGRP)
+    VLANy
+    routing między vlanmi
+    EtherChannel
+    konfiguracja FHRP
+    konfiguracja syslogu
+    konfiguracja NTP
+    konfiguracja AAA
+    konfiguracja serwera DHCP
+    dwie standardowe listy dostępu ACL
+    dwie rozszerzone listy dostępu ACL
+    zabezpieczenia przez atakami MAC
+    zabezpieczenia przez atakami VLAN
+    zabezpieczenia przez atakami DHCP
+    zabezpieczenia przez atakami STP
+    konfiguracja poziomów dostępowych na urządzeniach sieciowych
+
+
+
+
 ############################################################################################################
 ############################################################################################################
 SCHEMAT SIECI
