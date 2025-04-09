653a0a96421b6fbcd3d365cce717f3c6ed4b8e6a
CptTemplar1 <michal2001k@op.pl>
2025-04-09 18:42:02 +0200

    Dodano zabezpieczenie przed atakami MAC

--- KonfiguracjaSieci_opisowo.sh
+++ KonfiguracjaSieci_opisowo.sh
@@ -265,6 +265,17 @@ interface range FastEthernet0/1, FastEthernet0/2, FastEthernet0/4
  exit
 #######
 
+#ZABEZPIECZENIA PRZED ATAKAMI MAC
+#######
+interface range FastEthernet0/2, FastEthernet0/4
+ switchport mode access
+ switchport port-security
+ switchport port-security maximum 1
+ switchport port-security violation restrict
+ switchport port-security mac-address sticky
+ exit
+#######
+
 end
 write memory
 
